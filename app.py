0adf40041c3ed1068463ddb0945abcbc0b7bf257
Raghav Khatoria <raghavkhatoria42@gmail.com>
2021-10-11 22:32:09 +0530

    First Push

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pickle
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def showFile():
+    return render_template('forest.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    data = []
+    if request.form.to_dict()['region'] == 'southwest':
+        data.extend([0,0,0,1])
+    elif request.form.to_dict()['region'] == 'southeast':
+        data.extend([0,0,1,0])
+    elif request.form.to_dict()['region'] == 'northwest':
+        data.extend([0,1,0,0])
+    elif request.form.to_dict()['region'] == 'northeast':
+        data.extend([1,0,0,0])
+    data.extend([int(request.form.to_dict()['age']),int(request.form.to_dict()['sex'] == 'male'),float(request.form.to_dict()['bmi']),int(request.form.to_dict()['children']),int(request.form.to_dict()['smoker'] == 'yes')])
+    print(data)
+    newdata = [np.array(data)]
+    prediction = model.predict(newdata)
+    return render_template('forest.html',info=f"Interest : {prediction[0]}")
+
+if __name__ == '__main__':
+    app.run()
